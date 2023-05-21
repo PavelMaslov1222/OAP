36f58da3abca0492761f5dc5269c5c75b7d827ab
zalupa <134199447+PavelMaslov1222@users.noreply.github.com>
2023-05-21 21:51:16 +0500

    Add files via upload

---
+++ 1 билет/Билет номер 1 по ОАП/Билет номер 1 по ОАП/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Билет_номер_1_по_ОАП
+{
+    internal class Program
+    {
+        
+          
+        static void Main(string[] args)
+        {
+            
+            REG_MARK rEG_MARK = new REG_MARK();//Создаю объект класса чтобы можно было обращаться к созданному классу и его функциям 
+            string Path = "C:\\Users\\Houme-PC\\OneDrive\\Рабочий стол\\Все билеты по экзамену ОАП\\1 билет\\test1.txt";//Указываю путь к файлу 
+            string[] Mrk = File.ReadAllLines(Path);//Создали массив с типом данных стринг в него будут пемещенны все номера; Функция ReadAllLines Она просматривает файл и каждую строку файла помещает в отдельный объект массива 
+            foreach(string mmrk  in Mrk)//Через Цикл Foreach перебираю массив чтобы можно было работать с каждым элементом массива 
+            {
+                Console.WriteLine(mmrk+ ": "+rEG_MARK.CheckMark(mmrk));//вывожу результат программы 
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1 билет/Билет номер 1 по ОАП/Билет номер 1 по ОАП/REG_MARK.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Билет_номер_1_по_ОАП
+{
+    public class REG_MARK
+    {
+        public bool CheckMark(string mark)
+        {
+            string[] regions = new string[] { "74", "01", "174", "02", "03", "04", "96", "196", "197" };//Создал массив с доступными регионами которые могут быть на номерах автомобилей в России
+            List<char> chars = new List<char>() { 'Й', 'Ц', 'Г', 'Ш', 'Щ', 'З', 'Ъ', 'Ф', 'Ы', 'П', 'Л', 'Д', 'Ж', 'Э', 'Я', 'Ч', 'И', 'Ь', 'Б', 'Ю', 'Ё' };//Создали лист недопустимых букв в номере
+
+            string nomer = mark.Substring(0, 6);//создаю переменную которая показывает с какого момента и какой длины будет номер 
+            string regionNumber = mark.Substring(6);//создаю переменную которая показывает с какой момента будет начинаться регион
+
+            foreach (char c in chars)//Через цикл перебираю лист недопустимых букв
+            {
+                if (nomer.Contains(c)) return false;//условие усли переменная nomer содержит недопустимые символы то выдается false
+            }
+            if (!regions.Contains(regionNumber)) return false;//если в массиве регионов не находится регион который указан в номере то программа выдаст false 
+
+            return true;//то что будет выдаваться если не попадает под все условия
+        }
+    }
+}
