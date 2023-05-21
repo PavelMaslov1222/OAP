5b6877972849b8bfec1656f8c37eb7ecad17f9a3
PavelMaslov1222 <134199447+PavelMaslov1222@users.noreply.github.com>
2023-05-21 14:41:01 +0500

    Add files via upload
    здесь находится второй билет по ОАП в себя он включает текстовый документ и саму программу с созданных классом и функцией данного класса

---
+++ 2 билет/Сама программа со всем кодом/2 БИЛЕТ ПО ОАП/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//Подключил библиотеку для работы с текстовыми файлами 
+
+namespace _2_БИЛЕТ_ПО_ОАП
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = "C:\\Users\\Houme-PC\\OneDrive\\Рабочий стол\\test2.txt";//Путь к моему файлу на компьютере
+            string[] loginss = File.ReadAllLines(Path);//Создал массив с именем loginss и спомощью функции ReadAllLines, которая находится в классе File, она каждую строку в файле делает как отдельный элемент массива
+
+            REG_MARK rEG_MARK = new REG_MARK(); //Создал объект класса, чтобы у меня была возможность обращаться к созданному классу и его функциям 
+            foreach (string log in loginss)//Перебираю через foreach свой массив чтобы иметь доступ к каждому элементу массива через переменную типа стринг log
+            {
+                Console.WriteLine(log +": " + rEG_MARK.CheckLogin(log));//Вывожу на консоль свою реализованную функцию, сначала выводится сам логин потом двоиточие и дальше идет уже результат, подходит ли логин под критерии
+            }
+            Console.ReadLine();//Закрепляю консоль 
+        }
+    }
+}
---
+++ 2 билет/Сама программа со всем кодом/2 БИЛЕТ ПО ОАП/REG_MARK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_БИЛЕТ_ПО_ОАП
+{
+    public class REG_MARK
+    {
+        public bool CheckLogin(string login)
+        {
+            if (login.Length < 8)//Первое условие проверки логина, если он меньше 8 символов то программа сразу выдает False
+            {
+                return false;
+            }
+            if (login.Length >= 8)//Второе условие если логин больше 8 символов в длину то проверка идет дальше 
+            {
+                if (!char.IsDigit(login[0]))//Третье условие если первый сивол в логине не относится к категории десятичных цифр  то выдается true
+                {
+                    return true;
+                }
+            }
+            return false;//В остальных случаях выдает false
+        }
+    }
+}
